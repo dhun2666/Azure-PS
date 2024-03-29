ba2be7d30cab387eab544ba4e7c84ad26751c0cf
dhun2666 <88648235+dhun2666@users.noreply.github.com>
2024-03-29 13:50:02 +0900

    Update NsgToCsv.ps1

--- NsgToCsv.ps1
+++ NsgToCsv.ps1
@@ -1,6 +1,6 @@
 #command line input for NSG name, resource group
 
-$nsgname = Read-Host -Prompt 'Input your Firewall policy name'
-$resourcegroup = Read-Host -Prompt 'Input your NSG resourcegroup name'
+$nsgname = Read-Host -Prompt 'Input name of NSG'
+$resourcegroup = Read-Host -Prompt 'Input resourcegroup of NSG'
 
 $nsg = Get-AzNetworkSecurityGroup -Name $nsgname -ResourceGroupName $resourcegroup | Get-AzNetworkSecurityRuleConfig | Select-Object Name, @{n='Priority'; e={$_.Priority -join ' '}}, @{n='Protocol'; e={$_.Protocol -join ' '}}, @{n ='SourcePortRange'; e={$_.SourcePortRange -join ' '}}, @{n ='DestinationPortRange'; e={$_.DestinationPortRange -join ' '}}, @{n ='SourceAddressPrefix'; e={$_.SourceAddressPrefix -join ' '}}, @{n ='DestinationAddressPrefix'; e={$_.DestinationAddressPrefix -join ' '}}, Access, Direction, ProvisioningState | Export-Csv ".\$($nsgname).csv" -NoTypeInformation
